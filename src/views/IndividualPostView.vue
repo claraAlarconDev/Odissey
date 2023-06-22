15874503c7b242738cc412156ff6b6a5334e01a5
Melisa <melisa.barboza@hotmail.com>
2023-06-22 18:41:32 -0300

    individual post

--- src/views/IndividualPostView.vue
+++ src/views/IndividualPostView.vue
@@ -1,10 +1,25 @@
 <template>
   <ion-page>
     <ion-content>
-    <h1 class="title">Post individual</h1>
-     <h1 class="textos">{{this.post.titulo}}</h1>
-     <h1>{{this.post.id}} {{this.post.descripcion}} {{this.post.parrafo}}</h1>
+          <ion-img src="..\..\img\odissey 2.jpeg" class="imgpost"></ion-img>
+      <h2 class="textos">{{this.post.titulo}}</h2>
+      <ion-list>
+        
+        <div class="post-text">
+          {{this.post.descripcion}}
+        </div>
+        
+        <ion-item>
+          
+       </ion-item>
+       
+        <div class="post-text">
+          {{this.post.parrafo}}
+        </div>
+
+      </ion-list>
     </ion-content>
+    
   </ion-page>
 </template>
 
@@ -33,6 +48,45 @@ export default {
 }
 </script>
 
-<style>
 
+<style>
+.textos {
+  color: #3880ff;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 150%;
+  margin: 5% auto;
+  text-align: center;
+}
+
+.boton {
+  width: 90%;
+  margin-top: 5%;
+  margin-left: 5%;
+  margin-right: 5%;
+  font-size: 150%;
+  font-family: "Courier New", Courier, monospace;
+}
+
+.text-area {
+  width: 90%;
+  margin-top: 5%;
+  margin-left: 5%;
+  margin-right: 5%;
+  font-family: "Courier New", Courier, monospace;
+}
+
+.imgpost {
+  margin: 5% auto;
+  width: 30%;
+  max-width: 100px;
+  alt: centered;
+}
+
+.post-text {
+  color: black;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 100%;
+  width: 90%;
+  margin-left: 5%;
+}
 </style>
